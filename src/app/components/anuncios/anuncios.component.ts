0fb71dc937cbd876e8d0d9df5b724993344a7a8d
Ramiro Arechar <ramiro@arechar>
2017-12-27 00:14:37 -0600

    prueba anuncios

--- src/app/components/anuncios/anuncios.component.ts
+++ src/app/components/anuncios/anuncios.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnunciosComponent implements OnInit {
 
+  anuncios: any[] = [
+    {
+      titulo: "Pants Deportivos",
+      descripcion: "Alta resistencia e ideal para deportes",
+      fecha_publicacion: "2018-12-26",
+      img_src: "assets/imgs/pants.jpg",
+      precio: "$3,000.00",
+      categoria: "Promociones",
+      avatar_src: "assets/imgs/logo.png"
+    },
+    {
+      titulo: "Short Deportivo",
+      descripcion: "ideal para los que practican running",
+      fecha_publicacion: "2018-12-26",
+      img_src: "assets/imgs/short.jpg",
+      precio: "$1,000.00",
+      categoria: "Promociones",
+      avatar_src: "assets/imgs/logo.png"
+    },
+    {
+      titulo: "Conjunto Deportivo para Dama",
+      descripcion: "Bonito conjunto deportivo, unitalla.",
+      fecha_publicacion: "2018-12-26",
+      img_src: "assets/imgs/conjunto.jpg",
+      precio: "$2,000.00",
+      categoria: "Promociones",
+      avatar_src: "assets/imgs/logo.png"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
