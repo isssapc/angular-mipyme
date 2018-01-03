b75a27d2b0cf9308baf5b2326dd4460a9890b8d2
Ramiro Arechar <arechar@local.com>
2018-01-03 16:40:35 -0600

    clientes component

---
+++ src/app/components/clientes/clientes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html',
+  styleUrls: ['./clientes.component.scss']
+})
+export class ClientesComponent implements OnInit {
+
+
+  clientes: any = [
+    {
+      nombre: "José López",
+      email: "jose@hotmail.com",
+      telefono: "9613216548",
+    },
+    {
+      nombre: "Jorge Hernandez",
+      email: "jorge@hotmail.com",
+      telefono: "9613216548",
+    },
+    {
+      nombre: "Maria Perez",
+      email: "maria@hotmail.com",
+      telefono: "9613216548",
+    }
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
