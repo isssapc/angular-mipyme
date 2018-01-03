84ac83153b84b12b2ac8905ce4c151cdc2db7328
Ramiro Arechar <arechar@local.com>
2018-01-03 16:41:15 -0600

    pedidos component

---
+++ src/app/components/pedidos/pedidos.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import { ClienteDialogoComponent } from '../../dialogos/cliente-dialogo/cliente-dialogo.component';
+import { MatDialog } from '@angular/material';
+import { W_OK } from 'constants';
+import { PedidoDialogoComponent } from '../../dialogos/pedido-dialogo/pedido-dialogo.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pedidos',
+  templateUrl: './pedidos.component.html',
+  styleUrls: ['./pedidos.component.scss']
+})
+export class PedidosComponent implements OnInit {
+
+  pedidos: any = [
+    {
+      fecha: "12-12-2017 14:00hrs",
+      cliente: "Jorge Hernandez",
+    },
+    {
+      fecha: "12-12-2017 14:00hrs",
+      cliente: "José López",
+    },
+    {
+      fecha: "12-12-2017 14:00hrs",
+      cliente: "Maria Perez",
+    }
+  ]
+
+  constructor(
+    public dialog: MatDialog,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  verCliente() {
+
+    let dialogRef = this.dialog.open(ClienteDialogoComponent, {
+      data: {
+        nombre: "Jorge Hernandez",
+        telefono: "9612365458",
+        email: "jorge@hotmail.com",
+      },
+      width: '500px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    });
+
+  }
+
+
+  verPedido() {
+
+    let dialogRef = this.dialog.open(PedidoDialogoComponent, {
+      data: {
+
+        pedidos: [
+          {
+            producto: "Pantalon",
+            precio: "$1,000.00"
+          },
+          {
+            producto: "Pants Deportivos",
+            precio: "$1,000.00"
+          },
+          {
+            producto: "Tenis",
+            precio: "$1,500.00"
+          }
+        ],
+        total: "$3,500.00",
+
+
+      },
+      width: '500px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+    });
+
+  }
+
+  verPedidoCompleto() {
+    this.router.navigate(["/pedido"]);
+  }
+
+
+
+}
